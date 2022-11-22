ff22d1a3a20a5e2564ed5f8c9df5f573ed433266
presentKey <kim.hyeonjae4@gmail.com>
2022-11-23 01:24:31 +0900

    new: Implement remove toDoList

--- js/todo.js
+++ js/todo.js
@@ -2,12 +2,18 @@ const toDoForm = document.querySelector('#todo-form')
 const toDoInput = toDoForm.querySelector('input')
 const toDoList = document.querySelector('#todo-list')
 
+function deleteToDo(e) {
+  const li = e.target.parentElement
+  li.remove()
+}
+
 function paintTodo(newTodo) {
   const li = document.createElement('li')
   const span = document.createElement('span')
   span.innerText = newTodo
   const button = document.createElement('button')
   button.innerText = '❌'
+  button.addEventListener('click', deleteToDo)
   li.appendChild(span)
   li.appendChild(button)
   toDoList.appendChild(li)
